807fc7a8aa4ff7c39a76de75ce4dde35b9f87d48
Rien Maertens <maertensrien@gmail.com>
2016-09-14 10:06:05 +0200

    Notification when things go wrong

--- scripts/shot.sh
+++ scripts/shot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-user="rien"
-hostname="marnix"
+user="screenshot"
+hostname="gunnar"
 location="/srv/http/screenshots"
 webadress="http://i.rxn.be"
 
@@ -11,12 +11,16 @@ file="$(mktemp XXXX.png)"
 # Save the server adress to the clipboard
 echo "$webadress/$file" | xclip -sel clip
 # Take a screenshot with the user's arguments
-escrotum $* "$file" > /dev/null 2>&1 
+escrotum $* "$file" > /dev/null 2>&1
 # Make the file secure
 chmod 644 $file
 # Push the screenshot to the server
-scp -p $file $user@$hostname:$location/$file
-# Notify the user
-notify-send "Screenshot taken: $webadress/$file"
+if scp -p $file $user@$hostname:$location/$file
+then
+    # Notify the user
+    notify-send "Screenshot taken: $webadress/$file"
+else
+    notify-send -u critical "Failed to send screenshot to server."
+fi
 # Remove the temporary file
 rm -f $file
